ec828f7ab05fc5d26ec7de115f768c8dc58e628a
Lucas Sanches Fantacuci <lucasfantacuci@gmail.com>
2018-10-23 12:01:41 -0300

    Imagining the structure for messages workflow

--- src/metal/mod.rs
+++ src/metal/mod.rs
@@ -17,8 +17,25 @@ impl Metal {
             thread::spawn(move || {
                 let mut buffer: [u8; 2048] = [0; 2048];
                 stream.read(&mut buffer).unwrap();
-                println!("{}", String::from_utf8_lossy(&buffer[..]));
+                let request = Metal::parse_incomming_message(format!("{}", String::from_utf8_lossy(&buffer[..])));
+                let response = Response;
+                //self.dispatch_to_route(request, response);
+                stream.write(Metal::parse_output_message(response);)
             });
         }
     }
+
+    fn parse_incomming_message(message: String) -> Request {
+        println!("{}", message);
+        let x = Request;
+        x
+    }
+
+    fn parse_output_message(response: Response) -> &'static [u8; 98] {
+        b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello world</body></html>\r\n"
+    }
 }
+
+struct Request;
+
+struct Response;
